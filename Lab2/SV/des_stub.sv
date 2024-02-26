760194ae29c5c476cb9bf2cbfd9cae4fa08c1030
MaxKampy <94155235+MaxKampy@users.noreply.github.com>
2024-02-26 15:22:17 -0800

    Update des_stub.sv
    End of Day 1

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -27,16 +27,206 @@ module GenerateKeys (Key, SubKey1, SubKey2, SubKey3, SubKey4,
    output logic [47:0] SubKey14;
    output logic [47:0] SubKey15;
    output logic [47:0] SubKey16;
+   logic [27:0] c1;
+   logic [27:0] d1;
+   logic [27:0] c2;
+   logic [27:0] d2;
+   logic [27:0] c3;
+   logic [27:0] d3;
+   logic [27:0] c4;
+   logic [27:0] d4;
+   logic [27:0] c5;
+   logic [27:0] d5;
+   logic [27:0] c6;
+   logic [27:0] d6;
+   logic [27:0] c7;
+   logic [27:0] d7;
+   logic [27:0] c8;
+   logic [27:0] d8;
+   logic [27:0] c9;
+   logic [27:0] d9;
+   logic [27:0] c10;
+   logic [27:0] d10;
+   logic [27:0] c11;
+   logic [27:0] d11;
+   logic [27:0] c12;
+   logic [27:0] d12;
+   logic [27:0] c13;
+   logic [27:0] d13;
+   logic [27:0] c14;
+   logic [27:0] d14;
+   logic [27:0] c15;
+   logic [27:0] d15;
+   logic [27:0] c16;
+   logic [27:0] d16;
+
+
+	//Initial
+	PC1 I(key,c1,d1);
+
+
+	assign c2 = {c1[26:0],c1[27]};
+	assign d2 = {d1[26:0],d1[27]};
+	PC2 II(c2, d2, subkey1);
+
+
+	assign c3 = {c2[26:0],c2[27]};
+	assign d3 = {d2[26:0],d2[27]};
+	PC2 III(c3,d3,subkey2);
+
+
+	assign c4 = {c3[25:0],c3[27:26]};
+	assign d4 = {d3[25:0],d3[27:26]};
+	PC2 IV(c4,s4,subkey4);
+
+
+	assign c5 = {c4[25:0],c4[27:26]};
+	assign d5 = {d4[25:0],d4[27:26]};
+	PC2 V(c5,d5,subkey5);
+
+
+	assign c6 = {c5[25:0],c5[27:26]};
+	assign d6 = {d5[25:0],d5[27:26]};
+	PC2 VI(c6,d6,subkey6);
+
+
+	assign c7 = {c6[25:0],c6[27:26]};
+	assign d7 = {d6[25:0],d6[27:26]};
+	PC2 VII(c7,d7,subkey7);
+
+
+	assign c8 = {c7[25:0],c7[27:26]};
+	assign d8 = {d7[25:0],d7[27:26]};
+	PC2 VIII(c8,d8,subkey8);
+
+
+	assign c9 = {c8[25:0],c8[27:26]};
+	assign d9 = {d8[25:0],d8[27:26]};
+	PC2 IX(c9,d9,subkey9);
+
+
+	assign c10 = {c9[26:0],c9[27]};
+	assign d10 = {d9[26:0],d9[27]};
+	PC2 X(c10,d10,subkey10);
+
+
+	assign c11 = {c10[25:0],c10[27]};
+	assign d11 = {d10[25:0],d10[27]};
+	PC2 XI(c11,d11,subkey11);
+
+
+	assign c12 = {c11[25:0],c11[27]};
+	assign d12 = {d11[25:0],d11[27:26]};
+	PC2 XII(c12,d12,subkey12);
+
+
+	assign c13 = {c12[25:0],c12[27:26]};
+	assign d13 = {d12[25:0],d12[27:26]};
+	PC2 XIII(c13,d13,subkey13);
+
+
+	assign c14 = {c13[25:0],c13[27:26]};
+	assign d14 = {d13[25:0],d13[27:26]};
+	PC2 XIV(c14,d14,subkey14);
+
+
+	assign c15 = {c14[25:0],c14[27:26]};
+	assign d15 = {d14[25:0],d14[27:26]};
+	PC2 XV(c15,d15,subkey15);
+
+
+	assign c16 = {c15[26:0],c15[27]};
+	assign d16 = {d15[26:0],d15[27]};
+	PC2 XVI(c16,d16,subkey16);
+	
+
+
+
+
+
+
+
 
 endmodule // GenerateKeys
 
-module PC1 (key, left_block, right_block);
+module PC1 (key, c1, d1);
 
-   input logic [63:0]  key;
-   output logic [27:0] left_block;
-   output logic [27:0] right_block;
+   	input logic [63:0]  key;
+   	output logic [27:0] c1;
+	output logic [27:0] d1;
+	
+
 
    logic [55:0]        out_block;
+   //LEFT BLOCK
+	assign c1[27] = key[64-57];
+	assign c1[26] = key[64-49];
+	assign c1[25] = key[64-41];
+	assign c1[24] = key[64-33];
+	assign c1[23] = key[64-25];
+	assign c1[22] = key[64-17];
+	assign c1[21] = key[64-9];
+	assign c1[20] = key[64-1];
+	assign c1[19] = key[64-58];
+	assign c1[18] = key[64-50];
+	assign c1[17] = key[64-42];
+	assign c1[16] = key[64-34];
+	assign c1[15] = key[64-26];
+	assign c1[14] = key[64-18];
+	assign c1[13] = key[64-10];
+	assign c1[12] = key[64-2];
+	assign c1[11] = key[64-59];
+	assign c1[10] = key[64-51];
+	assign c1[9] = key[64-43];
+	assign c1[8] = key[64-35];
+	assign c1[7] = key[64-27];
+	assign c1[6] = key[64-19];
+	assign c1[5] = key[64-11];
+	assign c1[4] = key[64-3];
+	assign c1[3] = key[64-60];
+	assign c1[2] = key[64-52];
+	assign c1[1] = key[64-44];
+	assign c1[0] = key[64-36];
+
+   //RIGHT BLOCK
+	assign d1[27] = key[64-63];
+	assign d1[26] = key[64-55];
+	assign d1[25] = key[64-47];
+	assign d1[24] = key[64-39];
+	assign d1[23] = key[64-31];
+	assign d1[22] = key[64-23];
+	assign d1[21] = key[64-15];
+	assign d1[20] = key[64-7];
+	assign d1[19] = key[64-62];
+	assign d1[18] = key[64-54];
+	assign d1[17] = key[64-46];
+	assign d1[16] = key[64-38];
+	assign d1[15] = key[64-30];
+	assign d1[14] = key[64-22];
+	assign d1[13] = key[64-14];
+	assign d1[12] = key[64-6];
+	assign d1[11] = key[64-61];
+	assign d1[10] = key[64-53];
+	assign d1[9] = key[64-45];
+	assign d1[8] = key[64-37];
+	assign d1[7] = key[64-29];
+	assign d1[6] = key[64-21];
+	assign d1[5] = key[64-13];
+	assign d1[4] = key[64-5];
+	assign d1[3] = key[64-28];
+	assign d1[2] = key[64-20];
+	assign d1[1] = key[64-12];
+	assign d1[0] = key[64-4];
+
+
+	
+
+
+	//Shuffled Deck
+	//assign out_block[55:0] = {shiftedLeftBlock[27:0],shiftedRightBlock[27:0]};
+	
+
+   
 
 endmodule // PC1
 
@@ -45,6 +235,64 @@ module PC2 (left_block, right_block, subkey);
    input logic [27:0] left_block;
    input logic [27:0] right_block;
    output logic [47:0] subkey;
+   logic [55:0] combined;
+   assign combined[55:0] = {left_block[27:0],right_block[27:0]};
+
+
+   subkey[47] = combined[56-14];
+   subkey[46] = combined[56-17];
+   subkey[45] = combined[56-11];
+   subkey[44] = combined[56-24];
+   subkey[43] = combined[56-1];
+   subkey[42] = combined[56-5];
+   subkey[41] = combined[56-3];
+   subkey[40] = combined[56-28];
+   subkey[39] = combined[56-15];
+   subkey[38] = combined[56-6];
+   subkey[37] = combined[56-21];
+   subkey[36] = combined[56-10];
+   subkey[35] = combined[56-23];
+   subkey[34] = combined[56-19];
+   subkey[33] = combined[56-12];
+   subkey[32] = combined[56-4];
+   subkey[31] = combined[56-26];
+   subkey[30] = combined[56-8];
+   subkey[29] = combined[56-16];
+   subkey[28] = combined[56-7];
+   subkey[27] = combined[56-27];
+   subkey[26] = combined[56-20];
+   subkey[25] = combined[56-13];
+   subkey[24] = combined[56-2];
+   subkey[23] = combined[56-41];
+   subkey[22] = combined[56-52];
+   subkey[21] = combined[56-31];
+   subkey[20] = combined[56-37];
+   subkey[19] = combined[56-47];
+   subkey[18] = combined[56-55];
+   subkey[17] = combined[56-30];
+   subkey[16] = combined[56-40];
+   subkey[15] = combined[56-51];
+   subkey[14] = combined[56-45];
+   subkey[13] = combined[56-33];
+   subkey[12] = combined[56-48];
+   subkey[11] = combined[56-44];
+   subkey[10] = combined[56-49];
+   subkey[9] = combined[56-39];
+   subkey[8] = combined[56-56];
+   subkey[7] = combined[56-34];
+   subkey[6] = combined[56-53];
+   subkey[5] = combined[56-46];
+   subkey[4] = combined[56-42];
+   subkey[3] = combined[56-50];
+   subkey[2] = combined[56-36];
+   subkey[1] = combined[56-29];
+   subkey[0] = combined[56-32];
+
+
+
+
+
+
 
 endmodule // PC2
 
