9c0baa22547a5359cc12ed04fdb73beb7a53eb9d
MaxKampy <94155235+MaxKampy@users.noreply.github.com>
2024-04-05 11:51:00 -0700

    FINAL

--- Lab3/SV/clk_div.sv
+++ Lab3/SV/clk_div.sv
@@ -1,14 +1,21 @@
 module clk_div (input logic clk, input logic rst, output logic clk_en);
 
-   logic [23:0] clk_count;
+   logic [27:0] clk_count;
 
    always_ff @(posedge clk) begin
-      if (rst)
-	clk_count <= 24'h0;
+      //clk_en <= clk_en;
+      if (rst)begin
+	      clk_count <= 28'h0;
+         clk_en <= 1'b0;
+      end
+      else if (clk_count == 28'h3B9ACA0) begin
+         clk_count <= 28'b0;
+         clk_en <= ~clk_en;
+      end
       else
-	clk_count <= clk_count + 1;
-   end   
+	      clk_count <= clk_count + 1;
+         clk_en <= 1'b0;
+      end   
    
-   assign clk_en = clk_count[23];
-   
+
 endmodule // clk_div
--- Lab3/SV/fsm.sv
+++ Lab3/SV/fsm.sv
@@ -1,12 +1,12 @@
-module FSM (clk, reset, a, y);
+module FSM (clk, reset, a, b, y);
 
    input logic  clk;
    input logic  reset;
    input logic 	a;
-   
+   input logic b;
    output logic y;
 
-   typedef enum 	logic [1:0] {S0, S1, S2} statetype;
+   typedef enum 	logic [3:0] {S0, S1, S2, S3, S4, S5, S6, S7, S8, S9} statetype;
    statetype state, nextstate;
    
    // state register
@@ -17,25 +17,65 @@ module FSM (clk, reset, a, y);
    // next state logic
    always_comb
      case (state)
+
        S0: begin
-	  y <= 1'b0;	  
-	  if (a) nextstate <= S0;
-	  else   nextstate <= S1;
-       end
+	      y <= 6'b000000;
+        if(a && b)	  
+	      nextstate <= S7;
+
+        else if(b)
+        nexstate <= S4;
+        
+        else if(a)
+        nextstate <= S1;
+          end
+
        S1: begin
-	  y <= 1'b0;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S1;
-       end
+	      y <= 6'b001000;	  	  
+	      nextstate <= S2;
+          end
+
        S2: begin
-	  y <= 1'b1;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S0;
+	      y <= 6'b011000;;	  	  
+	      nextstate <= S3;
+          end
+
+       S3: begin
+        y <= 6'b111000;
+        nextstate <= S0;
        end
-       default: begin
-	  y <= 1'b0;	  	  
-	  nextstate <= S0;
+
+       S4: begin
+        y <= 6'b000100;
+        nextstate <= S5
        end
+
+       S5: begin
+        y <= 6'b000110;
+        nextstate <= S6;
+       end
+
+       S6: begin
+        y <= 6'b000111;
+        nextstate <= S0;
+       end
+
+       S7: begin
+        y <= 6'b001100;
+        nextstate <= S8;
+       end
+
+       S8:begin
+        y <= 6'b011110;
+        nextstate <= S9;
+       end
+
+        
+       S9: begin
+        y <= 6'b111111;
+        nextstate <= S0;
+       end
+
      endcase
    
 endmodule
